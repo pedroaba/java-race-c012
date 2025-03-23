b34befd2c90b61ce39b584dee572641d4f057356
Marco <marcorrditoro@gmail.com>
2025-03-23 18:27:52 -0300

    Cool changes 😎

--- src/main/java/pedroaba/java/race/ThreadRacing.java
+++ src/main/java/pedroaba/java/race/ThreadRacing.java
@@ -14,13 +14,13 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadRacing extends PApplet {
-    private static final int WIDTH = 1000;
+    private static final int WIDTH = 1200;
     private static final int HEIGHT = 800;
     private static final int TRACK_Y_START = 150;
     private static final int LANE_HEIGHT = 80;
     private static final int FINISH_LINE_X = 900;
-    private static final int CAR_WIDTH = 60;
-    private static final int CAR_HEIGHT = 30;
+    private static final int CAR_WIDTH = 40;
+    private static final int CAR_HEIGHT = 82;
 
     private Race race;
     private Dispatcher<Object> dispatcher;
@@ -33,9 +33,9 @@ public class ThreadRacing extends PApplet {
     private int countdownValue = 3;
     private long lastCountdownTime = 0;
 
-    // Imagens dos carros
-    private PImage beetleImg;
+    // Imagens
     private PImage ferrariImg;
+    private PImage beetleImg;
     private PImage lamboImg;
     private PImage bananaImg;
     private PImage boostImg;
@@ -55,53 +55,24 @@ public class ThreadRacing extends PApplet {
         smooth();
 
         // Carregando fontes
-        font = createFont("Monospace", 14, true); // TODO: Escolher uma fonte binita
+        font = createFont("monospace", 14, true); // TODO: Colocar uma fonte binita
         textFont(font);
 
-        // Carregando imagens
-        beetleImg = createCarImage(color(0, 150, 0));  // Verde para Beetle
-        ferrariImg = createCarImage(color(255, 0, 0)); // Vermelho para Ferrari
-        lamboImg = createCarImage(color(255, 255, 0)); // Amarelo para Lamborghini
+        ferrariImg = loadImage("src/images/Carro_1.png");  // Ferrari
+        beetleImg = loadImage("src/images/Carro_2.png");   // Beetle
+        lamboImg = loadImage("src/images/Carro_3.png");    // Lamborghini
+
+        // Redimensionando imagens
+        if (ferrariImg != null) ferrariImg.resize(CAR_WIDTH, CAR_HEIGHT);
+        if (beetleImg != null) beetleImg.resize(CAR_WIDTH, CAR_HEIGHT);
+        if (lamboImg != null) lamboImg.resize(CAR_WIDTH, CAR_HEIGHT);
 
         // Icones de poderes
-        bananaImg = createPowerImage(color(255, 255, 0)); // Amarelo para banana
-        boostImg = createPowerImage(color(0, 0, 255));    // Azul para boost
-        shellImg = createPowerImage(color(255, 0, 0));    // Vermelho para shell
+        bananaImg = createPowerImage(color(255, 255, 0)); // Banana
+        boostImg = createPowerImage(color(0, 0, 255));    // Boost
+        shellImg = createPowerImage(color(255, 0, 0));    // Shell
 
-        // Configurando a corrida
         setupRace();
-    }
-
-    private PImage createCarImage(int carColor) {
-        PImage img = createImage(CAR_WIDTH, CAR_HEIGHT, RGB);
-        img.loadPixels();
-        for (int i = 0; i < img.pixels.length; i++) {
-            int x = i % CAR_WIDTH;
-            int y = i / CAR_WIDTH;
-
-            // Forma basica de carro
-            if (x > 10 && x < CAR_WIDTH - 5 && y > 5 && y < CAR_HEIGHT - 5) {
-                img.pixels[i] = carColor;
-            } else if ((x > 5 && x < 10 && y > 10 && y < CAR_HEIGHT - 10) ||
-                    (x > CAR_WIDTH - 10 && x < CAR_WIDTH - 5 && y > 10 && y < CAR_HEIGHT - 10)) {
-                img.pixels[i] = carColor;
-            } else {
-                img.pixels[i] = color(0, 0, 0, 0); // Transparente
-            }
-
-            // Janelas
-            if (x > 20 && x < 50 && y > 8 && y < 12) {
-                img.pixels[i] = color(200, 200, 255);
-            }
-
-            // Rodas
-            if ((x > 10 && x < 20 && y > CAR_HEIGHT - 8 && y < CAR_HEIGHT - 3) ||
-                    (x > CAR_WIDTH - 25 && x < CAR_WIDTH - 15 && y > CAR_HEIGHT - 8 && y < CAR_HEIGHT - 3)) {
-                img.pixels[i] = color(0);
-            }
-        }
-        img.updatePixels();
-        return img;
     }
 
     private PImage createPowerImage(int powerColor) {
@@ -242,9 +213,6 @@ public class ThreadRacing extends PApplet {
         textAlign(CENTER, CENTER);
         text("Thread Racing", WIDTH / 2, 40);
 
-        // Desenha pista
-        drawTrack();
-
         // Countdown
         if (countdownStarted) {
             long currentTime = System.currentTimeMillis();
@@ -275,6 +243,8 @@ public class ThreadRacing extends PApplet {
                     text(countdownValue, WIDTH / 2, HEIGHT / 2);
                 }
             }
+        } else {
+            drawTrack();
         }
 
         // Desenha carros
@@ -290,7 +260,7 @@ public class ThreadRacing extends PApplet {
     }
 
     private void drawTrack() {
-        // Desenha fundo da pista
+        // Fundo da pista
         fill(100);
         rect(50, TRACK_Y_START - 20, WIDTH - 100, LANE_HEIGHT * carVisuals.size() + 40);
 
@@ -303,7 +273,7 @@ public class ThreadRacing extends PApplet {
         fill(255, 0, 0, 100);
         rect(FINISH_LINE_X, TRACK_Y_START - 20, 10, LANE_HEIGHT * carVisuals.size() + 40);
 
-        // Desenha lanes
+        // Lanes
         noStroke();
         for (int i = 0; i < carVisuals.size(); i++) {
             if (i % 2 == 0) {
@@ -326,28 +296,32 @@ public class ThreadRacing extends PApplet {
 
     private void drawCars() {
         for (CarVisual carVisual : carVisuals.values()) {
-            // Calcula posição X baseada na posição da corrida (0 a 100)
+            // Calcula X baseada na posição da corrida (0 a 100)
             float trackLength = FINISH_LINE_X - 60;
             float xPos = 60 + min((float)(carVisual.position / 100.0 * trackLength), trackLength);
             float yPos = TRACK_Y_START + carVisual.laneIndex * LANE_HEIGHT + LANE_HEIGHT / 2;
 
-            // Desenha sombra
+            // Sombra do carro
             fill(0, 0, 0, 50);
             noStroke();
             ellipse(xPos + 5, yPos + 15, 50, 20);
 
-            // Desenha o carro
+            // Desenha o carro rotacionado para a direita
+            pushMatrix();
+            translate(xPos, yPos);
+            rotate(HALF_PI); // Rotaciona 90 graus (sprite aponta para cima, queremos para a direita)
             imageMode(CENTER);
-            image(carVisual.image, xPos, yPos);
+            image(carVisual.image, 0, 0);
+            popMatrix();
 
-            // Desenha nome/velocidade ao lado do carro
+            // Nome e Velocidade ao lado do carro
             textAlign(LEFT, CENTER);
             textSize(14);
             fill(0);
-            text(carVisual.name + " [" + carVisual.threadId + "]", xPos + 40, yPos - 15);
-            text("Vel: " + nf((float)carVisual.speed, 1, 2), xPos + 40, yPos + 5);
+            text(carVisual.name + " [" + carVisual.threadId + "]", xPos + 50, yPos - 15);
+            text("Vel: " + nf((float)carVisual.speed, 1, 2), xPos + 50, yPos + 5);
 
-            // Se o carro terminou, desenha um marcador
+            // Marcador qunado terminar
             if (carVisual.finished) {
                 fill(0, 255, 0);
                 ellipse(xPos + 30, yPos - 15, 15, 15);
@@ -357,7 +331,7 @@ public class ThreadRacing extends PApplet {
                 text("✓", xPos + 30, yPos - 15);
             }
 
-            // TODO: Desenhar com forme o que está sendo printado no console
+            // TODO: desenhar com base nos poderes
             if (random(1) < 0.05 && !carVisual.finished) {
                 PImage powerImg;
                 int powerType = (int)random(3);
